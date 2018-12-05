263e178e12b74e121f0045985db10cdbc35ae33f
Michael Pieschel <sixtyhectares@yahoo.com>
2018-12-05 23:36:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 function init() {
   //put any page initialization/handlebars initialization here
+  let template = Handlebars.compile(document.getElementById("recipe-form-template").innerHTML);
+  Handlebars.registerHelper("displayIngredient", function(ingredient) {
+    return new Handlebars.SafeString('<li name="ingredientsList">' + ingredient + '</li>')
+ })
+ Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML)
 }
+
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
 })
+
+function handleSubmit() {
+
+}
+
+function displayEditForm() {
+
+}
